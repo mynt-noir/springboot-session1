02860e3dd8c6e067a783d5f615e07ad0f2a3856f
mynt-noir <nuanpatricia.serrano@gcash.com>
2025-07-18 17:08:21 +0800

    add: lab 8 functionalities

---
+++ springboot-session2/lab8/src/main/java/com/example/lab8/ProductController.java
@@ -0,0 +1,23 @@
+package com.example.lab8;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    // We will add methods here in the next steps
+}
